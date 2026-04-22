32a91ce3f3491873f24a5641b2a14641ef5201e0
Nikhil P Sasi <nikhilpsasi@gmail.com>
2026-04-22 22:24:46 +0530

    operators

---
+++ javascript-training/language-basics/operators.js
@@ -0,0 +1,7 @@
+let a=10;
+let b=10;
+console.log("Post increment:"+(a++));
+console.log("Next line of post increment:"+a);
+
+console.log("Pre increment:"+(++b));
+console.log("Next line of pre increment:"+b);
---
+++ javascript-training/language-basics/variable-declaration.js
@@ -0,0 +1,7 @@
+var a=10;
+let b=10;
+const c = 12;
+
+console.log(a);
+console.log(b);
+console.log(c);
