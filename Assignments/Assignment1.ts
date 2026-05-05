05716ad592c5d40cd608bb039aa8c4d92d2a4090
Nikhil P Sasi <nikhilpsasi@gmail.com>
2026-05-05 22:19:52 +0530

    Assignment 1

---
+++ Assignments/Assignment1.ts
@@ -0,0 +1,47 @@
+let celsius:number=25.5;
+let orderStatus:boolean=true;
+let phoneNumber:string ="123-456-7890";
+let money:number = 1000.50;
+let email:string = "john.doe@example.com";
+interface Coordinates {
+    latitude: number;
+    longitude: number;
+}
+let coordinates: Coordinates = { latitude: 37.7749, longitude: -122.4194 };
+let maritalStatus:boolean=true;
+let occupation:string = "Software Engineer";
+let color:string= "Blue";
+let year:number = 2023;
+let followersCount:number = 1000000;
+let movieRating:number = 7.5;
+let bloodType:string = "A";
+let bookTitle:string = "To Kill a Mockingbird";
+let employeeCount:number = 500;
+let timeOfEvent:string = "2.30 PM";
+let countryName:string = "United States";
+let eyeColor:string = "Brown";
+let birthPlace:string = "New York City";
+let distance:number = 200.5;
+
+
+console.log(`Temparature of a city in degree Celsius : ${celsius}`);
+console.log(`Whether a customer has placed an order : ${orderStatus}`);
+console.log(`Persons phone number : ${phoneNumber}`);
+console.log(`Amount of money in a customer's bank account : ${money}`);
+console.log(`Persons email address : ${email}`);
+console.log("Coordinates:", coordinates.latitude, coordinates.longitude);
+console.log(`Persons marital status : ${maritalStatus}`);
+console.log(`Persons occupation : ${occupation}`);
+console.log(`Persons favourite color : ${color}`);
+console.log(`Current year : ${year}`);
+console.log(`Number of followers on a social media platform : ${followersCount}`);
+console.log(`Rating of a movie : ${movieRating}`);
+console.log(`Persons blood type : ${bloodType}`);
+console.log(`Title of a book : ${bookTitle}`);
+console.log(`Number of employees in a company : ${employeeCount}`);
+console.log(`Time of an event : ${timeOfEvent}`);
+console.log(`Number of a country : ${countryName}`);
+console.log(`Persons eye color : ${eyeColor}`);
+console.log(`Persons birth place : ${birthPlace}`);
+console.log(`Distance between two cities : ${distance}`);
+
